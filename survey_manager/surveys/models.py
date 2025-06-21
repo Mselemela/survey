4b7eded36726d74d3b6751b08ad6a072047d18f3
Mpho <mselemela10@gmail.com>
2025-06-21 06:07:23 +0200

    django install

--- survey_manager/surveys/models.py
+++ survey_manager/surveys/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse # Import reverse
+from django.conf import settings
 
 # Create your models here.
