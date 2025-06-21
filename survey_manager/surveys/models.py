e64a7e7ce003123a25c162a16b9259f30315fc33
Mpho <mselemela10@gmail.com>
2025-06-21 07:36:25 +0200

    models class survey

--- survey_manager/surveys/models.py
+++ survey_manager/surveys/models.py
@@ -4,3 +4,17 @@ from django.urls import reverse # Import reverse
 from django.conf import settings
 
 # Create your models here.
+class Survey(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('surveys:survey_detail', args=[str(self.id)])
+
